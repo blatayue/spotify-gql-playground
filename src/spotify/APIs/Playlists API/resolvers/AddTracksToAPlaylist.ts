37f526d76665a95944a48b9145b39bf4c8f95b42
blatayue <morris.r.andrew@gmail.com>
2020-04-05 16:02:25 -0700

    update addTracksToAPlaylist

--- src/spotify/APIs/Playlists API/resolvers/AddTracksToAPlaylist.ts
+++ src/spotify/APIs/Playlists API/resolvers/AddTracksToAPlaylist.ts
@@ -1,11 +1,14 @@
 import refetch from "@hazelee/refetch";
-
+import fetch from "node-fetch";
 export const addTracksToPlaylist: addTracksToPlaylistT = async (
   parent,
   { tracks, playlist_id, position },
   context
-) => await refetch
-    .post(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks`, {
+) => {
+  const resp = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`,
+    {
+      method: "POST",
       headers: {
         authorization: `Bearer ${context.spotify.getAccessToken()}`,
         content_type: "application/json"
@@ -13,9 +16,11 @@ export const addTracksToPlaylist: addTracksToPlaylistT = async (
       body: position // optional
         ? JSON.stringify({ uris: tracks, position })
         : JSON.stringify({ uris: tracks })
-    })
-    .json();
-
+    }
+  );
+  if (resp.status !== 201) throw new Error((await resp.json()).error.message);
+  return await resp.json();
+};
 type addTracksToPlaylistT = (
   parent: any, // query root
   args: {
