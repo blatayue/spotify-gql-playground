784f86afeffdaaa904e8a5c1752f27ca346284d1
Andrew Morris <morris.r.andrew@gmail.com>
2020-04-16 02:15:03 +0000

    add missed type fields

--- src/spotify/APIs/Playlists API/spotifyObjectTypeDefs/PlaylistObject.ts
+++ src/spotify/APIs/Playlists API/spotifyObjectTypeDefs/PlaylistObject.ts
@@ -3,12 +3,15 @@ import { gql } from "apollo-server-micro";
 export const PlaylistObject = gql`
   type PlaylistObject {
     collaborative: Boolean
+    description: String
     external_urls: ExternalUrlObject
+    followers: FollowersObject
     href: String
     id: String
     images: [ImageObject]
     name: String
     owner: PublicUserObject
+    primary_color: String
     public: Boolean
     snapshot_id: String
     tracks: [TrackObject]
